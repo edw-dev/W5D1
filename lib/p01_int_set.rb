9d555712991df7b19ad4f05df80d1e9986a46720
mcountryman <countryman.marco@gmail.com>
2021-11-01 13:58:40 -0400

    started maxintset

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -1,11 +1,16 @@
 class MaxIntSet
   def initialize(max)
+    @max = max
+    @store = Array.new(max,false)
   end
 
   def insert(num)
+    raise "Out of bounds" if num < 0 || num > max
+    @store[num] = true
   end
 
   def remove(num)
+    
   end
 
   def include?(num)
